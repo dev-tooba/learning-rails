6fb3f257189e15f8ca7e44abe6cee1264fecc9a5
Tooba Ahmed <tooba.ahmed@fliclicks.com>
2021-10-20 20:01:05 +0500

    Did a lot with viwes

--- app/models/concerns/default_page_content.rb
+++ app/models/concerns/default_page_content.rb
@@ -1,8 +1,10 @@
 module DefaultPageContent
-    icluded do 
+    extend ActiveSupport::Concern
+    
+    included do 
     before_action :set_title
     end
-    
+
     def set_title
         @page_title = "Training for Ruby | "
     end
